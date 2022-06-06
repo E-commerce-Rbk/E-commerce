ce934fd427a3b02a0d74bcb946b610c54ec71009
mehdi <100924042+mehdiEnnabli@users.noreply.github.com>
2022-06-06 22:39:34 +0100

    signup login jwt and client api done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express  from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import cookieParser from 'cookie-parser';
 
 import productRoutes from './routes/product.js'
 import userRoutes from'./routes/user.js'
@@ -11,6 +12,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser())
 
 const CONNECTION_URL ='mongodb+srv://rbk:rbk123456789@ecommerce.kwvvj.mongodb.net/?retryWrites=true&w=majority'
 const PORT=process.env.PORT || 5000;
