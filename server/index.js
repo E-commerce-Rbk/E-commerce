713d783331f8cdba40b9806003e2e25489ea7426
medmahfoudhi <elmahfoudhi.med92@gmail.com>
2022-06-07 11:05:22 +0200

    sorry

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-import express  from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import cartRoutes from "./routes/cart.js";
 
-import productRoutes from './routes/product.js'
+import productRoutes from "./routes/product.js";
 const app = express();
 
-app.use(bodyParser.json({limit:"30mb",extended:true}))
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
-app.use(express.json())
-app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json());
+app.use(cors());
 
-const CONNECTION_URL ='mongodb+srv://rbk:rbk123456789@ecommerce.kwvvj.mongodb.net/?retryWrites=true&w=majority'
-const PORT=process.env.PORT || 5000;
-app.use('/product',productRoutes)
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>app.listen(PORT,()=>console.log(`Server runing on port:${PORT}`)))
-.catch((err)=>console.log(err.message))
+const CONNECTION_URL =
+  "mongodb+srv://rbk:rbk123456789@ecommerce.kwvvj.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+app.use("/product", productRoutes);
+app.use("/cart", cartRoutes);
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server runing on port:${PORT}`))
+  )
+  .catch((err) => console.log(err.message));
