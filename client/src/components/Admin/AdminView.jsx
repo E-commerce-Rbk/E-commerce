b9ec5292219d92d6f44b8672905b2738a6094982
Ahmed-Azzabi <azzabiahmed98@gmail.com>
2022-06-12 13:16:16 +0100

    admin

--- client/src/components/Admin/AdminView.jsx
+++ client/src/components/Admin/AdminView.jsx
@@ -1,188 +1,69 @@
 import React from "react";
 import AdminSidebar from "../AdminSideBar/AdminSideBar.jsx";
 import "./admin.css";
+import axios from "axios";
 
-function AdminView() {
-  return (
-    <div className="admin-container ">
-      <AdminSidebar></AdminSidebar>
-      <div class="bg-white">
-        <div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 class="sr-only">Products</h2>
-          <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            <a href="#" class="group">
-              <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
-                  alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                  class="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-              <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
-              <button
-                type="button"
-                class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  class="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
-                Edit
-              </button>
-            </a>
+export default class AdminView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
 
-            <a href="#" class="group">
-              <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg"
-                  alt="Olive drab green insulated bottle with flared screw lid and flat top."
-                  class="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 class="mt-4 text-sm text-gray-700">Nomad Tumbler</h3>
-              <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-              <button
-                type="button"
-                class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  class="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
-                Edit
-              </button>
-            </a>
+  componentDidMount() {
+    axios.get("http://localhost:5000/product").then((res) => {
+      this.setState({ data: res.data });
+    });
+  }
 
-            <a href="#" class="group">
-              <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg"
-                  alt="Person using a pen to cross a task off a productivity paper card."
-                  class="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 class="mt-4 text-sm text-gray-700">Focus Paper Refill</h3>
-              <p class="mt-1 text-lg font-medium text-gray-900">$89</p>
-              <button
-                type="button"
-                class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  class="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
-                Edit
-              </button>
-            </a>
+  render() {
+    console.log(this.state);
+    return (
+      <div className="admin-container">
+        <AdminSidebar></AdminSidebar>{" "}
+        <div className="bg-white">
+          <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+            <h2 className="sr-only">Products</h2>
 
-            <a href="#" class="group">
-              <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                <img
-                  src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg"
-                  alt="Hand holding black machined steel mechanical pencil with brass tip and top."
-                  class="w-full h-full object-center object-cover group-hover:opacity-75"
-                />
-              </div>
-              <h3 class="mt-4 text-sm text-gray-700">
-                Machined Mechanical Pencil
-              </h3>
-              <p class="mt-1 text-lg font-medium text-gray-900">$35</p>
-              <button
-                type="button"
-                class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  class="-ml-1 mr-2 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
-                Edit
-              </button>
-            </a>
+            <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+              {this.state.data.map((product) => (
+                <a key={product.id} href={product.href} className="group">
+                  <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                    <img
+                      style={{ height: "300px", width: "300px" }}
+                      src={product.imageUrl}
+                      alt={product.imageAlt}
+                      className="w-full h-full object-center object-cover group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 className="mt-4 text-sm text-gray-700">
+                    {product.productName}
+                  </h3>
+                  <p className="mt-1 text-lg font-medium text-gray-900">
+                    {product.price} TND
+                  </p>
+                  <button
+                    type="button"
+                    class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  >
+                    <svg
+                      class="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                    </svg>
+                    Edit
+                  </button>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
-      {/* <div>
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <form action="#" method="POST">
-              <div class="shadow sm:rounded-md sm:overflow-hidden">
-                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
-                  <div class="grid grid-cols-3 gap-6">
-                    <div class="col-span-3 sm:col-span-2">
-                      <label
-                        for="company-website"
-                        class="block text-sm font-medium text-gray-700"
-                      >
-                        {" "}
-                        Add product{" "}
-                      </label>
-                      <div class="mt-1 flex rounded-md shadow-sm">
-                        <input
-                          type="text"
-                          name="company-website"
-                          id="company-website"
-                          class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-                          placeholder="Product name.."
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  <div>
-                    <label
-                      for="about"
-                      class="block text-sm font-medium text-gray-700"
-                    >
-                      {" "}
-                      Details{" "}
-                    </label>
-                    <div class="mt-1">
-                      <textarea
-                        id="about"
-                        name="about"
-                        rows="3"
-                        class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                        placeholder="Product details..."
-                      ></textarea>
-                    </div>
-                  </div>
-                </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div> */}
-    </div>
-  );
+    );
+  }
 }
-
-export default AdminView;
